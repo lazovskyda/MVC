4d76013d40ec2f789eaf747c6b3cb64ff14604e5
lazovskyda <LazovskyDA@qulix.com>
2018-04-24 12:05:41 +0300

    some small refactoring

--- src/main/frontend/nbrb/src/app/app.component.ts
+++ src/main/frontend/nbrb/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import {CurrencyService} from "../services/currency.service";
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [CurrencyService]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent{
   title = 'app';
--- src/main/frontend/nbrb/src/app/currency/currency.component.ts
+++ src/main/frontend/nbrb/src/app/currency/currency.component.ts
@@ -4,7 +4,8 @@ import {CurrencyService} from "../../services/currency.service";
 @Component({
   selector: 'app-currency',
   templateUrl: './currency.component.html',
-  styleUrls: ['./currency.component.scss']
+  styleUrls: ['./currency.component.scss'],
+  providers: [CurrencyService]
 })
 
 export class CurrencyComponent implements OnInit {
@@ -17,21 +18,21 @@ export class CurrencyComponent implements OnInit {
   ngOnInit() {
     this.currencyService.getCurrency('USD').subscribe( currency => {
       this.currencies.set(currency.currencyId, currency);
-      console.log(this.currencies.get('USD'));
+      // console.log(this.currencies.get('USD'));
     });
 
     this.currencyService.getCurrency('EUR').subscribe( currency => {
       this.currencies.set(currency.currencyId, currency);
-      console.log(this.currencies.get('EUR'));
+      // console.log(this.currencies.get('EUR'));
     });
 
     this.currencyService.getCurrency('RUB').subscribe( currency => {
       this.currencies.set(currency.currencyId, currency);
-      console.log(this.currencies.get('RUB'));
-      console.log(this.getKeys(this.currencies));
+      // console.log(this.currencies.get('RUB'));
+
     });
   }
-  getKeys(currencies){
+  getValues(currencies){
     return Array.from(currencies.values());
   }
 }
